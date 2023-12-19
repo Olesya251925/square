066a49ffd7815198ad1c177cc638fc30e7eb43b6
олеся кириллова <olesya.kiril2014@gmail.com>
2023-12-19 20:12:58 +0700

    Добавьте файлы проекта.

---
+++ Метод наименьших квадратов/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace Метод_наименьших_квадратов
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.plotView1 = new OxyPlot.WindowsForms.PlotView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите функцию:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите n:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(44, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Рассчитать ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(96, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(192, 112);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Генерировать данные";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(69, 150);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Загрузить из EXCEL";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(382, 12);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(215, 237);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.UseWaitCursor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(612, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(268, 237);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // plotView1
+            // 
+            this.plotView1.Location = new System.Drawing.Point(12, 255);
+            this.plotView1.Name = "plotView1";
+            this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView1.Size = new System.Drawing.Size(868, 409);
+            this.plotView1.TabIndex = 10;
+            this.plotView1.Text = "plotView1";
+            this.plotView1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(162, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Кол-во строк";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(242, 61);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(36, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 667);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.plotView1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ Метод наименьших квадратов/Program.cs
@@ -0,0 +1,454 @@
+﻿using System;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+using OxyPlot.Axes;
+using MathNet.Symbolics;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Text;
+using System.Globalization;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace Метод_наименьших_квадратов
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            MainForm mainForm = new MainForm();
+            Application.Run(mainForm);
+        }
+    }
+
+    public partial class MainForm : Form
+    {
+        private Label labelFunction;
+        private TextBox textBoxFunction;
+        private Label labelN;
+        private TextBox textBoxN;
+        private Button calculateButton;
+        private Button clearButton;
+        private Button generButton;
+        private Button excelButton;
+        private TextBox localPointsTextBox;
+        private DataGridView dataGridView;
+        private PlotView plotView;
+        private Label line;
+        private TextBox textBox5;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.Size = new System.Drawing.Size(1000, 800);
+        }
+
+        private void InitializeComponent()
+        {
+            labelFunction = new Label();
+            labelFunction.AutoSize = true;
+            labelFunction.Location = new System.Drawing.Point(9, 26);
+            labelFunction.Name = "labelFunction";
+            labelFunction.Size = new System.Drawing.Size(100, 13);
+            labelFunction.TabIndex = 0;
+            labelFunction.Text = "Введите функцию:";
+
+            textBoxFunction = new TextBox();
+            textBoxFunction.Location = new System.Drawing.Point(115, 23);
+            textBoxFunction.Name = "textBox1";
+            textBoxFunction.Size = new System.Drawing.Size(134, 20);
+            textBoxFunction.TabIndex = 1;
+
+            labelN = new Label();
+            labelN.AutoSize = true;
+            labelN.Location = new System.Drawing.Point(12, 62);
+            labelN.Name = "label2";
+            labelN.Size = new System.Drawing.Size(61, 13);
+            labelN.TabIndex = 2;
+            labelN.Text = "Введите n:";
+
+            textBoxN = new TextBox();
+            textBoxN.Location = new System.Drawing.Point(96, 62);
+            textBoxN.Name = "textBox2";
+            textBoxN.Size = new System.Drawing.Size(44, 20);
+            textBoxN.TabIndex = 3;
+            
+            line = new Label();
+            line.AutoSize = true;
+            line.Location = new System.Drawing.Point(162, 65);
+            line.Name = "label3";
+            line.Size = new System.Drawing.Size(73, 13);
+            line.TabIndex = 11;
+            line.Text = "Кол-во строк";
+
+            textBox5 = new TextBox();
+            textBox5.Location = new System.Drawing.Point(242, 61);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new System.Drawing.Size(36, 20);
+            textBox5.TabIndex = 13;
+
+            calculateButton = new Button();
+            calculateButton.Location = new System.Drawing.Point(3, 112);
+            calculateButton.Name = "button1";
+            calculateButton.Size = new System.Drawing.Size(84, 23);
+            calculateButton.TabIndex = 4;
+            calculateButton.Text = "Рассчитать ";
+            calculateButton.UseVisualStyleBackColor = true;
+            calculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+
+            clearButton = new Button();
+            clearButton.Location = new System.Drawing.Point(96, 112);
+            clearButton.Name = "button2";
+            clearButton.Size = new System.Drawing.Size(80, 23);
+            clearButton.TabIndex = 5;
+            clearButton.Text = "Очистить";
+            clearButton.UseVisualStyleBackColor = true;
+            clearButton.Click += new System.EventHandler(this.ClearButton_Click);
+
+            generButton = new Button();
+            generButton.Location = new System.Drawing.Point(192, 112);
+            generButton.Name = "button3";
+            generButton.Size = new System.Drawing.Size(158, 23);
+            generButton.TabIndex = 6;
+            generButton.Text = "Генерировать данные";
+            generButton.UseVisualStyleBackColor = true;
+            generButton.Click += GenerateDataButton_Click;
+
+           excelButton = new Button();
+            excelButton.Location = new System.Drawing.Point(69, 150);
+            excelButton.Name = "button4";
+            excelButton.Size = new System.Drawing.Size(120, 23);
+            excelButton.TabIndex = 7;
+            excelButton.Text = "Загрузить из EXCEL";
+            excelButton.UseVisualStyleBackColor = true;
+            excelButton.Click += ExcelButton_Click;
+
+            localPointsTextBox = new TextBox();
+            localPointsTextBox.Location = new System.Drawing.Point(382, 12);
+            localPointsTextBox.Multiline = true;
+            localPointsTextBox.Name = "textBox3";
+            localPointsTextBox.Size = new System.Drawing.Size(215, 237);
+            localPointsTextBox.TabIndex = 8;
+            localPointsTextBox.UseWaitCursor = true;
+
+            dataGridView = new DataGridView();
+            dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.Location = new System.Drawing.Point(612, 12);
+            dataGridView.Name = "dataGridView";
+            dataGridView.Size = new System.Drawing.Size(268, 237);
+            dataGridView.TabIndex = 9;
+
+            plotView = new PlotView();
+            plotView.Location = new System.Drawing.Point(12, 255);
+            plotView.Name = "plotView";
+            plotView.PanCursor = System.Windows.Forms.Cursors.Hand;
+            plotView.Size = new System.Drawing.Size(868, 409);
+            plotView.TabIndex = 10;
+            plotView.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            plotView.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            plotView.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(892, 667);
+            Controls.Add(plotView);
+            Controls.Add(dataGridView);
+            Controls.Add(localPointsTextBox);
+            Controls.Add(excelButton);
+            Controls.Add(generButton);
+            Controls.Add(clearButton);
+            Controls.Add(calculateButton);
+            Controls.Add(textBoxN);
+            Controls.Add(labelN);
+            Controls.Add(textBoxFunction);
+            Controls.Add(labelFunction);
+            Controls.Add(line);
+            Controls.Add(textBox5);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Проверяем наличие данных в DataGridView
+                if (dataGridView.Rows.Count == 0)
+                {
+                    MessageBox.Show("Добавьте данные в DataGridView.", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                int degree = int.Parse(textBoxN.Text);
+
+                double[,] data = new double[dataGridView.Rows.Count, 2];
+                for (int i = 0; i < dataGridView.Rows.Count; i++)
+                {
+                    data[i, 0] = Convert.ToDouble(dataGridView.Rows[i].Cells["XColumn"].Value);
+                    data[i, 1] = Convert.ToDouble(dataGridView.Rows[i].Cells["YColumn"].Value);
+                }
+
+                // Вызываем метод наименьших квадратов для нахождения коэффициентов
+                PolynomialCoefficients coefficients = LeastSquaresMethod(degree, data);
+
+                // Выводим коэффициенты функции в текстовое поле
+                StringBuilder coefficientsString = new StringBuilder("Коэффициенты функции: \r\n");
+                for (int i = 0; i < coefficients.Coefficients.Length; i++)
+                {
+                    coefficientsString.Append($"a{i} = {coefficients.Coefficients[i]:F2}\r\n  ");
+                }
+
+                localPointsTextBox.Text = coefficientsString.ToString();
+
+                // Строим график
+                PlotModel plotModel = new PlotModel();
+
+                // Добавляем точки из DataGridView на график
+                ScatterSeries scatterSeries = new ScatterSeries { MarkerType = MarkerType.Circle };
+                for (int i = 0; i < data.GetLength(0); i++)
+                {
+                    scatterSeries.Points.Add(new ScatterPoint(data[i, 0], data[i, 1]));
+                }
+                plotModel.Series.Add(scatterSeries);
+
+                // Находим минимальное и максимальное значения X для построения функции
+                double minX = data[0, 0];
+                double maxX = data[0, 0];
+                for (int i = 1; i < data.GetLength(0); i++)
+                {
+                    minX = Math.Min(minX, data[i, 0]);
+                    maxX = Math.Max(maxX, data[i, 0]);
+                }
+
+                // Добавляем аппроксимированную функцию на график
+                FunctionSeries functionSeries = new FunctionSeries(x => CalculateApproximation(coefficients.Coefficients, x, degree), minX, maxX, 0.1);
+                plotModel.Series.Add(functionSeries);
+
+                // Добавляем сетку на график
+                plotModel.Axes.Add(new OxyPlot.Axes.LinearAxis { MajorGridlineStyle = LineStyle.Solid, MinorGridlineStyle = LineStyle.Dot, Position = AxisPosition.Bottom });
+                plotModel.Axes.Add(new OxyPlot.Axes.LinearAxis { MajorGridlineStyle = LineStyle.Solid, MinorGridlineStyle = LineStyle.Dot, Position = AxisPosition.Left });
+
+                plotView.Model = plotModel;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при расчете коэффициентов: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private double CalculateApproximation(double[] coefficients, double x, int degree)
+        {
+            double result = 0;
+            for (int i = 0; i <= degree; i++)
+            {
+                result += coefficients[i] * Math.Pow(x, i);
+            }
+            return result;
+        }
+
+        private Func<double, double> ParseFunction(string functionText)
+        {
+            // Реализуйте парсинг функции и возвращение делегата Func<double, double>
+            try
+            {
+                // Пример парсинга, замените его на реальный код
+                SymbolicExpression expression = SymbolicExpression.Parse(functionText);
+                return x => Convert.ToDouble(expression.Compile("x").Invoke(x).ToString());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ошибка при парсинге функции: " + ex.Message);
+            }
+        }
+
+        private void GenerateDataButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int rowCount;
+                if (!int.TryParse(textBox5.Text, out rowCount) || rowCount <= 0)
+                {
+                    MessageBox.Show("Введите корректное число строк.", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Очищаем существующие данные в DataGridView и добавляем столбцы, если их нет
+                ClearDataGridView();
+
+                if (dataGridView.Columns.Count == 0)
+                {
+                    dataGridView.Columns.Add("XColumn", "X");
+                    dataGridView.Columns.Add("YColumn", "Y");
+                }
+
+                // Генерируем новые данные и выводим их в DataGridView
+                Random random = new Random();
+                for (int i = 0; i < rowCount; i++)
+                {
+                    double x = Math.Round(random.NextDouble() * 10, 2); // Генерация случайных значений X с двумя знаками после запятой
+                    double y = Math.Round(random.NextDouble() * 10, 2); // Генерация случайных значений Y с двумя знаками после запятой
+
+                    dataGridView.Rows.Add(x, y); // Добавляем строку с x и y
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при генерации данных: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ClearDataGridView()
+        {
+            // Очищаем значения в столбцах XColumn и YColumn в DataGridView
+            dataGridView.Rows.Clear();
+        }
+
+        private double CalculateApproximation(Func<double, double> function, double x, int degree)
+        {
+            double result = 0;
+            for (int i = 0; i <= degree; i++)
+            {
+                result += function(x) * Math.Pow(x, i);
+            }
+            return result;
+        }
+
+        private void ExcelButton_Click(object sender, EventArgs e)
+        {
+            LoadFromExcelAndApply();
+        }
+
+        private Tuple<string[,], int> ExportExcel()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.DefaultExt = "*.xls;*.xlsx";
+            ofd.Filter = "Файлы Excel (Spisok.xlsx)|*.xlsx";
+
+            if (!(ofd.ShowDialog() == DialogResult.OK))
+                return Tuple.Create<string[,], int>(null, 0);
+
+            Excel.Application ObjWorkExcel = new Excel.Application();
+            Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(ofd.FileName);
+            Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1];
+            var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);
+
+            int lastRow = (int)lastCell.Row;
+            int lastColumn = (int)lastCell.Column;
+
+            // Изменяем размер массива list, чтобы вместить данные
+            string[,] list = new string[lastRow, lastColumn];
+
+            // Заполняем массив list данными из Excel
+            for (int i = 0; i < lastRow; i++)
+            {
+                for (int j = 0; j < lastColumn; j++)
+                {
+                    // Преобразуем текст ячейки в строковом представлении в тип double
+                    if (double.TryParse(ObjWorkSheet.Cells[i + 1, j + 1].Text.ToString(),
+                                        System.Globalization.NumberStyles.Any,
+                                        CultureInfo.InvariantCulture,
+                                        out double cellValue))
+                    {
+                        // Преобразуем значение ячейки в строку с учетом возможного минуса и дробной части
+                        list[i, j] = cellValue.ToString("G2");
+                    }
+                    else
+                    {
+                        // Если не удалось преобразовать, сохраняем текст ячейки как есть
+                        list[i, j] = ObjWorkSheet.Cells[i + 1, j + 1].Text.ToString();
+                    }
+                }
+            }
+
+            ObjWorkBook.Close(false, Type.Missing, Type.Missing);
+            ObjWorkExcel.Quit();
+            GC.Collect();
+
+            return Tuple.Create(list, lastRow);
+        }
+
+        private void LoadFromExcelAndApply()
+        {
+            Tuple<string[,], int> excelData = ExportExcel();
+
+            if (excelData.Item2 > 0)
+            {
+                string[,] list = excelData.Item1;
+                int rowCount = excelData.Item2;
+
+                // Очищаем существующие данные в DataGridView
+                ClearDataGridView();
+
+                // Заполняем DataGridView данными из Excel
+                for (int i = 0; i < rowCount; i++)
+                {
+                    dataGridView.Rows.Add(list[i, 0], list[i, 1]); // Здесь предполагается, что у вас есть столбцы "XColumn" и "YColumn"
+                }
+            }
+        }
+
+        public struct PolynomialCoefficients
+        {
+            public double[] Coefficients;
+        }
+        private PolynomialCoefficients LeastSquaresMethod(int degree, double[,] data)
+        {
+            int rowCount = data.GetLength(0);
+
+            // Создаем матрицу A и вектор B для системы уравнений
+            double[,] A = new double[degree + 1, degree + 1];
+            double[] B = new double[degree + 1];
+
+            // Заполняем матрицу A и вектор B
+            for (int i = 0; i <= degree; i++)
+            {
+                for (int j = 0; j <= degree; j++)
+                {
+                    A[i, j] = 0;
+                    for (int k = 0; k < rowCount; k++)
+                    {
+                        A[i, j] += Math.Pow(data[k, 0], i + j);
+                    }
+                }
+
+                B[i] = 0;
+                for (int k = 0; k < rowCount; k++)
+                {
+                    B[i] += data[k, 1] * Math.Pow(data[k, 0], i);
+                }
+            }
+
+            // Решаем систему уравнений (Ax = B) для нахождения коэффициентов
+            Matrix<double> matrixA = DenseMatrix.OfArray(A);
+            Vector<double> vectorB = Vector<double>.Build.Dense(B);
+            Vector<double> result = matrixA.Solve(vectorB);
+
+            return new PolynomialCoefficients { Coefficients = result.ToArray() };
+        }
+
+        private double[] SolveSystemOfEquations(double[,] A, double[] B)
+        {
+            return new double[A.GetLength(0)];
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            textBoxN.Clear();
+            textBoxFunction.Clear();
+            localPointsTextBox.Text = string.Empty;
+
+            // Очищаем значения в столбцах "XColumn" и "YColumn" в DataGridView
+            foreach (DataGridViewRow row in dataGridView.Rows)
+            {
+                row.Cells["XColumn"].Value = null;
+                row.Cells["YColumn"].Value = null;
+            }
+        }
+
+    }
+}
